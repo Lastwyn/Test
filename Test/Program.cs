68b947905cb68c7b00b3f18d77057ecfddcc96d2
Lastwyn <113887330+Lastwyn@users.noreply.github.com>
2022-09-26 10:04:45 +0200

    2.0

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,6 +10,13 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adjon meg az első egész számot");
+            int elsoszam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adjon meg a második egész számot");
+            int masodikszam = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.ReadKey();
         }
     }
 }
